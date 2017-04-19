fc944fc750d6350c9180d8b9925cf457df968892
gamontec <gamontec@uc.cl>
2017-04-19 00:54:25 -0300

    update

---
+++ sesiones/sesion_2/slides/ejemplos/ejemplo_1_1.js
@@ -0,0 +1,7 @@
+function setup(){
+  createCanvas(300,300);
+}
+
+function draw(){
+  rect(150,150,90,90);
+}
---
+++ sesiones/sesion_2/slides/ejemplos/ejemplo_1_2.js
@@ -0,0 +1,7 @@
+function setup(){
+  createCanvas(300,200);
+}
+
+function draw(){
+  ellipse(150,100,90,90);
+}
