a832e37fa3d26f78cc67e639329e278dbe358826
gamontec <gamontec@uc.cl>
2017-05-11 17:27:27 -0300

    sesion3

---
+++ sesiones/sesion_3/ejemplos/ejemplo_2_1.js
@@ -0,0 +1,11 @@
+function setup(){
+  createCanvas(480,120);
+}
+
+function draw(){
+  background(204);
+  rect(150,150,90,90);
+  ellipse(75, 100, 130, 130);
+  ellipse(175, 100, 130, 130);
+  ellipse(275, 100, 130, 130);
+}
